6fc8510f93ad0641b132dbdecf6bf017b0d8b276
Athar Ramzan <atharramzan.ofi@gmail.com>
2025-09-18 23:02:27 +0530

    Time: 1 ms (12.38%) | Memory: 42.9 MB (26.56%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -13,17 +13,51 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
 class Solution {
-   public int maxDepth(TreeNode root) {
+    public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
 
-        // Recursively find depth of left and right subtrees
-        int lh = maxDepth(root.left);
-        int rh = maxDepth(root.right);
+        int depth = 0;
 
-        // Current depth = 1 (for root) + max of left and right depths
-        return 1 + Math.max(lh, rh);
+        while (!q.isEmpty()) {
+            int size = q.size();  
+
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.remove();
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+
+            depth++;  
+        }
+
+        return depth;
     }
 }
